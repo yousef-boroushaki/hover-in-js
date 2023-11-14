92201739fca1af6953145a2f247b1ec1828e213f
yousef boroushaki <yousef@gmail.com>
2023-11-14 22:41:47 +0330

    file added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+var image = document.getElementById("image");
+
+image.addEventListener("mouseover" , function(){
+    image.src = "image/bed1.jpg";
+})
+
+image.addEventListener("mouseout" , function(){
+    image.src = "image/bed.jpg";
+})
+
